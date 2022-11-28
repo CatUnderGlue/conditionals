d906f0f386fe544d91272bb7b0291c33ebd85ff3
CatUnderGlue <warmonds1996@gmail.com>
2022-11-28 19:06:53 +0500

    Task 3 and some change in Task 2

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,28 @@ public class Main {
 
         // Задание 2
         System.out.println("Задание 2");
-        boolean flag = false;
-        if (age >= 24 && !flag){
-            flag = true;
+        if (age >= 24){
             System.out.println("Вы окончили университет и можете искать первую работу.");
         }
-        if (age >= 18 && !flag){
-            flag = true;
+        if (age >= 18 && age <= 23){
             System.out.println("Вы закончили школу и можете отправляться в университет.");
         }
-        if (age >= 7 && !flag){
-            flag = true;
+        if (age >= 7 && age <= 17){
             System.out.println("Вы явно ходите в школу.");
         }
 
+        // Задание 3
+        System.out.println("Задание 3");
+        int peopleInTrain = 66;
+        int totalCapacity = 102;
+        if (peopleInTrain == totalCapacity){
+            System.out.println("Вагон полностью забит");
+        }
+        if (peopleInTrain >= 60 && peopleInTrain <= 101){
+            System.out.println("Остались только стоячие места");
+        }
+        if (peopleInTrain < 60 && peopleInTrain > 0){
+            System.out.println("Имеются как стоячие, так и сидячие места");
+        }
     }
 }
